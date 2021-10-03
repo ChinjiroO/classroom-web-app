972acfce97897c53ac04d78148d9ccc45a10987f
chinjiro <techinseoikrathok@gmail.com>
2021-10-03 22:27:05 +0700

    deploy 7

--- server/databases/connect.js
+++ server/databases/connect.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require("mongodb");
 const MONGODB_URI =
   "mongodb://chinjiro:Chinchin1452@cluster0.aj7dr.mongodb.net/go_room?retryWrites=true&w=majority";
-const client = new MongoClient(MONGODB_URI, {
+const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
