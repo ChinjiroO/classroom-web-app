d0ac9230d5ebbd9b84bc0897f1d6e2f7daa69d95
chinjiro <techinseoikrathok@gmail.com>
2021-10-03 20:32:41 +0700

    deploy2

--- server/app.js
+++ server/app.js
@@ -11,9 +11,10 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
+  app.use(express.static(path.join("../client/build")));
+
   app.get("*", function (request, response) {
-    response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+    response.sendFile(path.join(__dirname, "../client/build", "index.html"));
   });
 }
 
